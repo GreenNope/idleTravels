40f7fc53741f90347b2e9a03cd09240a924907cd
GreenNope <tlash1000@gmail.com>
2017-09-08 16:14:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var gold = 0
 var speed = 1
-var biomedist = Math.floor((Math.random() * 999) + 1)
+var biomeDist = Math.floor((Math.random() * 999) + 1)
+var currentBiome = biomes[1]
+
 
 function tick() {
     distance = distance + speed
