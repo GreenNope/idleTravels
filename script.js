b5846d8f697ac20a30229ea4517483fa79aa3114
GreenNope <tlash1000@gmail.com>
2017-09-14 17:45:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var gold = 0
 var speed = 1
 var biomeDist = Math.floor((Math.random() * 999) + 1)
 var currentBiome = biomes.forest
+var speedboost = 0.001
 document.getElementById("biome").innerHTML = currentBiome.name
 
 function tick() {
     distance = distance + speed
-    speed = speed + speed * 0.001
+    speed = speed + speed * speedboost
+    document.getElementById("speed").innerHTML = speed
 }
     
