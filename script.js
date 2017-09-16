072301d2107e915b2d0702b1181450c73d005639
GreenNope <tlash1000@gmail.com>
2017-09-16 15:22:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var gold = 0
 var speed = 1
 var biomeDist = Math.floor((Math.random() * 251) + 500)
-var currentBiome = biomes.forest
+var currentBiome = biomes.[0]
 var speedboost = 0.01
 var distance = 0
 document.getElementById("biome").innerHTML = currentBiome.name
