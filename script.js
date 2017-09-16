d4146cfb13ea450c6a188c613be21be278958bee
GreenNope <tlash1000@gmail.com>
2017-09-16 16:26:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ var speed = 1
 var biomeDist = Math.floor((Math.random() * 251) + 500)
 var currentBiome = biomes.forest
 var speedboost = 0.01
-var distance = 0
+var distance = 0'
+var walking = true
 document.getElementById("biome").innerHTML = currentBiome.name
 
 function tick() {
-    distance = distance + speed
-    speed = (Math.floor((speed + speed * speedboost) * 100))/100
+    if walking = true {
+        distance = distance + speed
+        speed = (Math.floor((speed + speed * speedboost) * 100))/100
+    } else {
+        var totalChance = 0
+        for (var mat in currentBiome.materials) {
+            totalChance += mat.chance
+        }
+    }
     document.getElementById("speed").innerHTML = speed
-    document.getElementById("biomeDist").innerHTML = biomeDist - distance
+    biomeDist = biomeDist - distance
+    document.getElementById("biomeDist").innerHTML = biomeDist
 }
 
 window.setInterval(tick, 1000) 
