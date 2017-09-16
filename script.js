7b85ebd01fa9ea6084269ed4b57bf9f8db08aea6
GreenNope <tlash1000@gmail.com>
2017-09-16 17:20:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var biomeDist = Math.floor((Math.random() * 251) + 500)
 var currentBiome = biomes.forest
 var speedboost = 0.01
 var distance = 0
-var walking = true
+var walking = false
 document.getElementById("biome").innerHTML = currentBiome.name
 
 function tick() {
