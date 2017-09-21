7af6e7a54c5ac66795cbb3fd52123793cade3924
GreenNope <tlash1000@gmail.com>
2017-09-20 20:14:59 -0400

    Update biomeData.js

--- biomeData.js
+++ biomeData.js
@@ -2,9 +2,10 @@ const biomes = {
     {
         name:"Forest",
         chance: 50,
-        materials: [
-            {name: "wood", chance: 50}, {name:"leaves", chance: 50}
-        ]
+    },
+    {
+        name:"Desert",
+        chance: 50,
     }
 }
     
